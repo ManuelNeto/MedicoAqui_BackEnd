5169a960b78b7903ebfc111b5781dd0993ad2f12
MANUEL SEVERINO DA SILVA NETO 114211270 <manuelssn@lcc3-011.lcc.ufcg.edu.br>
2018-09-26 13:54:47 -0300

    Api doc Swagger

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -5,12 +5,14 @@
 
 const express = require('express');
 const router = express.Router();
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./../../config/swagger");
 const user = require('../user/user.route');
 const medicalAppointment = require('../medicalAppointment/medicalAppointment.route');
 
 router.use('/user', user);
 router.use('/medicalAppointment', medicalAppointment);
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 router.get('/', function (req, res) {
